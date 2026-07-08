31ae5c5c15e2676c5e56f1db424ce5aed76ba724
Karthik Boddeti <108723702+Timmystickman@users.noreply.github.com>
2026-07-08 19:36:07 +0530

    Add files via upload

---
+++ DIPLOMA STUDENT INFORMATION MANAGEMENT SYSTEM.py
@@ -0,0 +1,30 @@
+print("=====WELCOME TO DIPLOMA STUDENT INFORMATION MANAGEMENT SYSTEM=====")
+
+print("PLEASE ENTER THE DETAILS ASKED BELOW IN THE SPACE BELOW")
+name    = input("NAME: ")
+age     = int(input("AGE : "))
+dob = (input("DOB: "))
+branch  = input("YOUR BRANCH : ")
+college = input("YOUR COLLEGE: ")
+joining_year = int(input("JOINED YEAR: "))
+
+print(f"""
+=========================================================== 
+              DIPLOMA STUDENT INFORMATION
+===========================================================              
+          NAME            :    {name}
+          AGE             :    {age}
+          DATE OF BIRTH   :    {dob}
+          BRANCH          :    {branch}
+          COLLEGE         :    {college}
+          JOINED YEAR     :    {joining_year}
+                    END OF REPORT
+ THANKS FOR CHOOSING OUR STUDENT INFORMATION MANAGEMENT SYSTEM      
+                     
+ =========================================================""")       
+ 
+ 
+ 
+ 
+ 
+ 
