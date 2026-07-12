c62ae1b258ed87538127b65db793a40ab21472d3
Karthik Boddeti <108723702+Timmystickman@users.noreply.github.com>
2026-07-12 21:41:26 +0530

    Add files via upload

---
+++ COUNTDOWN (prototype).py
@@ -0,0 +1,12 @@
+print("=========COUNTDOWN TIMER==========")
+num = int(input("Enter the number to start countdown : "))
+
+for i in range(num , 0 , -1) :
+    
+    print(f"{i}")
+
+print("🚀 BLAST OFF")    
+    
+print("================================================================")    
+     
+    
