8bb5fe353a8f4b859c8b1a33f96e70789e690994
Karthik Boddeti <108723702+Timmystickman@users.noreply.github.com>
2026-07-15 10:26:31 +0530

    Add files via upload

---
+++ PASSWORD STRENGTH CHECKER.py
@@ -0,0 +1,7 @@
+print("=========PASSWORD STRENGTH CHECKER=========")
+pas = input("Enter the password : ")
+if len(pas) >= 8 :
+    print("✅ THE PASSWORD IS STRONG")
+else :
+    print("❌THE PASSWORD IS WEAK (IT SHOULD ATLEAST CONTAIN 8 CHARACTERS)")   
+print("===========================================")     
